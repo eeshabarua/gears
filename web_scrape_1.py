e7e830b21a74a48ada61216f5e8ac368a12cac23
Eesha <eesha.barua@gmail.com>
2021-10-24 02:06:13 -0500

    Made Demo of Webscraping + Filtration

--- web_scrape_1.py
+++ web_scrape_1.py
@@ -1,6 +1,24 @@
+from bs4 import BeautifulSoup
 import requests
 
-URL = "https://www.causeiq.com/organizations/"
+# read the HTML
+URL = "https://www.causeiq.com/organizations/securities-investor-protection-corporation,520910763/"
 page = requests.get(URL)
 
-print(page.content)
+# store the HTML
+soup = BeautifulSoup(page.content, "lxml")
+
+# print raw HTML
+# print(soup.prettify())
+
+# scrape and print text from HTML classes
+course_tags = soup.find_all('div', class_='org-title-bar')
+
+for course_tag in course_tags:
+    course_name = course_tag.h1.text
+    course_id = course_tag.h1.text.split()[-1]
+    print(course_name)
+    print(course_id)
+
+# present info in a sentence
+print(f'{course_name} contains the ID {course_id}')
