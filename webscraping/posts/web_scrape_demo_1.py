bb53914f384eda1c9a840414cd66aa9e4252670a
Eesha <eesha.barua@gmail.com>
2021-10-24 10:29:54 -0500

    Fixed web_scrape_demo_1

--- webscraping/posts/web_scrape_demo_1.py
+++ webscraping/posts/web_scrape_demo_1.py
@@ -1,93 +1,93 @@
-from bs4 import BeautifulSoup, element
+from bs4 import BeautifulSoup
 import requests
-import pandas as pd
 
 # this demo retrieves all jobs posted few days ago from timesjobs.com
 
-def find_jobs():
-    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
-    # prints raw HTML file
-    # print(html_text)
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 
-    # create beautifulsoup instance and make it provide HTML
-    soup = BeautifulSoup(html_text, 'lxml')
+# prints raw HTML file
+# print(html_text)
 
-    # in browser -- catch an element and inspect all of them
-    # jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-    # for job in jobs:
-    #     print(job)
+# create beautifulsoup instance and make it provide HTML
+soup = BeautifulSoup(html_text, 'lxml')
+# print(soup.prettify)
 
-    # for the first single instance of the element
-    # job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
-    # print(job)
+# in browser -- catch an element and inspect all of them
+# jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+# for job in jobs:
+#     print(job)
 
-    # you want to search an attribute specifically within our element
-    # company_name = job.find('h3', class_='joblist-comp-name').text
-    # print(company_name)
+# for the first single instance of the element
+# job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+# print(job)
 
-    # formatting - delete whitespace
-    # company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-    # print(company_name)
+# you want to search an attribute specifically within our element
+# company_name = job.find('h3', class_='joblist-comp-name').text
+# print(company_name)
 
-    # search narrower attribute within the class we scraped - skill
-    # skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
-    # print(skill)
+# formatting - delete whitespace
+# company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+# print(company_name)
 
-    # pretty print statement
-    # print(f'''
-    #     Company Name: {company_name}
-    #     Skill Set: {skill}
-    # ''')
+# search narrower attribute within the class we scraped - skill
+# skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
+# print(skill)
 
-    # encapsulated element - use .span technique
-    # job_post_date = job.find('span', class_='sim-posted').span.text
-    # print(job_post_date)
+# pretty print statement
+# print(f'''
+#     Company Name: {company_name}
+#     Skill Set: {skill}
+# ''')
 
-    # now with multiple job elements 
-    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+# encapsulated element - use .span technique
+# job_post_date = job.find('span', class_='sim-posted').span.text
+# print(job_post_date)
 
-    # pull code under this for loop so we can do the same for all jobs on the website
-    # for job in jobs:
-    #     company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-    #     skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
-    #     job_post_date = job.find('span', class_='sim-posted').span.text
-    #     print(job_post_date)
+# now with multiple job elements 
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+# pull code under this for loop so we can do the same for all jobs on the website
+# for job in jobs:
+#     company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+#     skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
+#     job_post_date = job.find('span', class_='sim-posted').span.text
+#     print(job_post_date)
+    
+#     # print
+#     print(f'''
+#     Company Name: {company_name}
+#     Skill Set: {skill}
+#     ''')
+
+#     print('')
+#     # WOW!!!
+
+# MISSION - filter job posts that are not published a few days ago
+
+for job in jobs:
+    job_post_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in job_post_date:
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
         
-    #     # print
-    #     print(f'''
-    #     Company Name: {company_name}
-    #     Skill Set: {skill}
-    #     ''')
+        # print(job_post_date)
+        
+        print
+        print(f'''
+        Company Name: {company_name}
+        Skill Set: {skill}
+        ''')
 
-    #     print('')
-    #     # WOW!!!
+        # print('')
+        # DOUBLE WOW!!!
 
-    # mission - filter job posts that are not published a few days ago
-
-    for index, job in enumerate(jobs):
-        job_post_date = job.find('span', class_='sim-posted').span.text
-        if 'few' in job_post_date:
-            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-            skill = job.find('span', class_ ='srp-skills').text.replace(' ', '')
-            
-            # print(job_post_date)
-            
-            # print
-            # print(f'''
-            # Company Name: {company_name}
-            # Skill Set: {skill}
-            # ''')
-
-            # print('')
-            # DOUBLE WOW!!!
-
-            # write to the file
-            # first arg is an index, second is a permission
-            # with open(f'posts/{index}.txt', 'w') as f:
-            #     # pretty print
-            #     f.write(f"Company Name: {company_name}")
-            #     f.write(f"Required Skills: {skill}")
-            # # print confirmation message
-            # print(f'Filed saved: {index}')
+        # write to the file
+        # first arg is an index, second is a permission
+        # with open(f'posts/{index}.txt', 'w') as f:
+        #     # pretty print
+        #     f.write(f"Company Name: {company_name}")
+        #     f.write(f"Required Skills: {skill}")
+        # # print confirmation message
+        # print(f'Filed saved: {index}')
 
